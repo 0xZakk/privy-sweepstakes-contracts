91be5ec3859a0e7df7064587c91daa0c4c8c3d43
Zakk <zakk@hawthorne.io>
2023-04-10 16:09:39 -0500

    fix minted check; get contracts compiling

--- src/Token.sol
+++ src/Token.sol
@@ -1,26 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "solmate/src/tokens/ERC20.sol";
-import "solmate/src/auth/Owned.sol";
+import "solmate/tokens/ERC20.sol";
+import "solmate/auth/Owned.sol";
 
-/// @title SweepstakeToken
+/// @title SweepstakesToken
 /// @author 0xZakk (https://twitter.com/0xZakk)
 /// @notice This is a single-use token meant solely for the Privy Public Goods Sweepstakes. When users sign up, they will receive 10 tokens, which they can use to vote on who should win the sweepstakes.
 /// @dev This contract is based on Solmate's ERC20 and Owned contracts. Most actions are limited to just the owner (like mint and burn). Furthermore, most transfers are blocked, unless they are to a whitelisted recipient. That is to prevent sybil attacks where someone accumulates a lot of tokens and to prevent someone setting up a liquidity pool.
-contract SweepstakeToken is ERC20, Owned {
+contract SweepstakesToken is ERC20, Owned {
+    mapping(address => bool) public whitelist;
+    mapping(address => bool) public minted;
+
     constructor(
       string memory _name,
       string memory _symbol,
       address _owner)
-    ERC20(_name, _symbol, 0) {}
+    ERC20(_name, _symbol, 0) Owned(_owner) {}
 
     /// @notice Mint 10 tokens to a new Privy user
     /// @dev This function is only callable by the owner. A user should only ever receive 10 tokens once.
     /// @param _to The address of the user to mint tokens to
     function mint(address _to) public onlyOwner {
-        require(balanceOf(_to) == 0, "Already minted")
+        require(minted[_to] == false, "User already minted");
         _mint(_to, 10);
+
+        minted[_to] = true;
     }
 
     /// @notice Burn tokens from a user
@@ -68,15 +73,17 @@ contract SweepstakeToken is ERC20, Owned {
     /// @param _to The address of the recipient
     /// @param _amount The amount of tokens to transfer
     function transfer(address _to, uint256 _amount) public override returns (bool) {
-        require(whitelist[_recipient], "Transfer not allowed");
-        return super.transfer(_recipient, _amount);
+        require(whitelist[_to], "Transfer not allowed");
+        return super.transfer(_to, _amount);
     }
 
     /// @notice Transfer tokens from one address to another
     /// @dev We override the transferFrom function to ensure that only whitelisted addresses can receive tokens. This prevents users from accumulating tokens to a single address, setting up a liquidity pool, or doing anything other than voting in the sweepstakes.
-    /// @param Documents a parameter just like in doxygen (must be followed by parameter name)
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to transfer
     function transferFrom(address _from, address _to, uint256 _amount) public override returns (bool) {
-        require(whitelist[_recipient], "Transfer not allowed");
+        require(whitelist[_to], "Transfer not allowed");
         return super.transferFrom(_from, _to, _amount);
     }
 }
