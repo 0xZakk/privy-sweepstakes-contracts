0df46e0342dab54a0f31c3210a592abda2f6ce55
Zakk <zakk@hawthorne.io>
2023-04-19 14:23:20 -0500

    stub methods for payout and voting

--- src/Payout.sol
+++ src/Payout.sol
@@ -5,5 +5,10 @@ import {IPayoutStrategy} from "allo/payoutStrategy/IPayoutStrategy.sol";
 
 contract SweepstakesPayout is IPayoutStrategy {
 
+  function isDistributionSet() override public view returns (bool) {
+    return true;
+  }
+
+  function updateDistribution(bytes calldata _encodedDistribution) override external {}
 }
 
--- src/Voting.sol
+++ src/Voting.sol
@@ -5,4 +5,7 @@ import {IVotingStrategy} from "allo/votingStrategy/IVotingStrategy.sol";
 
 contract SweepstakesVoting is IVotingStrategy {
 
+  function vote(bytes[] calldata _encodedVotes, address _voterAddress) override external payable {
+
+  }
 }
