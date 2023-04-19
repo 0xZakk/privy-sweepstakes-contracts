cc9d7e7b9d988834e34788c6c613ee8878c587c5
Zakk <zakk@hawthorne.io>
2023-04-19 13:35:54 -0500

    add stubs for tests

---
+++ test/Relayer.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/* Placeholder for testing the round and strategy with the Gelato relayer */
+
+import "forge-std/Test.sol";
+
+contract RelayerTest is Test {}
---
+++ test/Token.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+contract TokenTest is Test {
+
+    // @TODO: testing minting and transfer blocking works
+}
